f0a662caae43e7162ca51ef63b3acbc98bb18359
Rrhul0 <rr025765@gmail.com>
2023-04-12 12:50:22 +0530

    removed default css and added tailwind css to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react'
-import './App.css'
 
 import io from 'socket.io-client'
 
@@ -23,26 +22,69 @@ function App() {
         }
     })
 
-    function vote(who: 'cat' | 'dog') {
-        console.log(who, voted)
+    function vote(who: 'cat' | 'dog' | 'none') {
         if (voted === who) return
-        if (voted !== 'none') {
-            socket.emit('unvote', voted)
-        }
+        //if changing vote or removing the vote first need to unvote
+        if (voted !== 'none' || who === 'none') socket.emit('unvote', voted)
+
         setVoted(who)
         socket.emit('vote', who)
     }
 
+    const votesPercent = {
+        cat: votesCat + votesDog !== 0 ? ((votesCat * 100) / (votesCat + votesDog)).toFixed(2) : 0,
+        dog: votesCat + votesDog !== 0 ? ((votesDog * 100) / (votesCat + votesDog)).toFixed(2) : 0,
+    }
+
     return (
-        <div className='App'>
-            <button className='box' onClick={() => vote('cat')}>
-                Cat {votesCat}
+        <main className='flex flex-col w-screen h-screen justify-center gap-6 bg-slate-200'>
+            <h1 className='text-5xl font-extrabold text-center'>Vote for</h1>
+            <div className='flex items-center justify-center gap-4 h-1/3'>
+                <button
+                    className={
+                        'flex flex-col gap-2 items-center justify-center aspect-square h-full rounded-lg border-2 relative overflow-hidden ' +
+                        `${voted === 'cat' ? ' border-green-400 ' : ' '}`
+                    }
+                    onClick={() => vote('cat')}
+                >
+                    <h2 className='text-lg font-semibold'>Cat</h2>
+                    <div className='font-light text-sm'>
+                        {votesCat} votes · {votesPercent.cat}%
+                    </div>
+                    <div
+                        style={{ height: votesPercent.cat + '%' }}
+                        className='absolute left-0 right-0 bottom-0 bg-pink-400 bg-opacity-50 rounded transition-all duration-700'
+                    ></div>
+                </button>
+                <div>OR</div>
+
+                <button
+                    className={
+                        'flex flex-col gap-2 items-center justify-center aspect-square h-full rounded-lg border-2 relative overflow-hidden ' +
+                        `${voted === 'dog' ? 'border-green-400' : ''}`
+                    }
+                    onClick={() => vote('dog')}
+                >
+                    <h2 className='text-lg font-semibold'>Dog</h2>
+                    <div className='font-light text-sm'>
+                        {votesDog} votes · {votesPercent.dog}%
+                    </div>
+                    <div
+                        style={{ height: votesPercent.dog + '%' }}
+                        className={
+                            'absolute left-0 right-0 bottom-0 bg-pink-400 bg-opacity-50 rounded transition-all duration-700'
+                        }
+                    ></div>
+                </button>
+            </div>
+            <h2 className='text-center text-lg font-semibold'>Total {votesCat + votesDog} votes</h2>
+            <button
+                onClick={() => vote('none')}
+                className='self-center focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900'
+            >
+                Remove your vote
             </button>
-            <div>OR</div>
-            <button className='box' onClick={() => vote('dog')}>
-                Dog {votesDog}
-            </button>
-        </div>
+        </main>
     )
 }
 
